ef1d455caabec9f8f8edb8d540f249a5aa19abc3
GlitchYou <7gatosoutracasa@gmail.com>
2021-08-19 12:36:17 -0300

    recreate api

--- js/playnimes/api.js
+++ js/playnimes/api.js
@@ -1,30 +1,9 @@
-const HOST = '152.44.32.124';
-const TOKEN  = 'VERoNS9wOXh2U0BiNlRZaExbZFBgL3gkJXBYd3Yk';
-const KEY = '1p4vo5hz1592mvvy';
+const HOST = ''
 
-
-function httpRequest(url, headers = {}, callback) {
-  const xhttp = new XMLHttpRequest();
-  xhttp.open("GET", url);
-
-  Object.keys(headers).forEach((key) => {
-    xhttp.setRequestHeader(key, headers[key]);
-  })
-
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      callback(JSON.parse(this.response))
-    }
-  }
-
-  xhttp.send();
-}
-
-function api(path, callback = console.log) {
-  const headers = {
-    'Authorization': TOKEN,
-    'Accept': 'application/json',
-  }
-  return httpRequest(`http://${HOST}${path}`, headers, callback);
+function api(path, callback) {
+  fetch(`http://${HOST}/${path}`)
+    .then((res) => res.json())
+    .then((data) => callback(data))
+    .catch(console.error)
 }
 
